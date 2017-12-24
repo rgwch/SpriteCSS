99afc142a1086fab2d51618f80f758f70ebdafa5
Jhovanni <ejhova@gmail.com>
2017-12-23 22:54:23 -0600

    Fix: plugin de validaciones disponible en todos los ambiente

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,18 @@ import {Aurelia} from 'aurelia-framework'
 import environment from './environment';
 
 export function configure(aurelia: Aurelia) {
-  aurelia.use
-    .standardConfiguration()
-    .feature('resources');
+    aurelia.use
+        .standardConfiguration()
+        .feature('resources')
+        .plugin('aurelia-validation');
 
-  if (environment.debug) {
-    aurelia.use.developmentLogging();
-  }
+    if (environment.debug) {
+        aurelia.use.developmentLogging();
+    }
 
-  if (environment.testing) {
-      aurelia.use.plugin('aurelia-testing').plugin('aurelia-validation');
-  }
+    if (environment.testing) {
+        aurelia.use.plugin('aurelia-testing');
+    }
 
-  aurelia.start().then(() => aurelia.setRoot());
+    aurelia.start().then(() => aurelia.setRoot());
 }
